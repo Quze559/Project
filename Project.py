5102c607735ce9dfb7d255829f263911de9d44df
Quze559 <76464415+Quze559@users.noreply.github.com>
2021-04-26 22:50:56 +0700

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,19 +1,27 @@
 from tkinter import *
+import datetime
 
 root = Tk()
 root.geometry("1280x1024")
 f_top=Frame(root)
 f_middle=Frame(root)
 f_bot=Frame(root)
-label1=Label(f_top, text="Место для текущего времени", font="Arial 12", width=50, height=15, bg="green")
-label2=Label(f_top, text="Место для текущей даты", font="Arial 12", width=50, height=15, bg="lightgreen")
+
+a = datetime.datetime.today()
+
+label1=Label(f_top , font="Arial 12", width=50, height=15, bg="green")
+label1.config(text=str(a.strftime("%m/%d/%Y")))
+label2=Label(f_top, font="Arial 12", width=50, height=15, bg="lightgreen")
+label2.config(text=str(a.strftime("%m/%d/%Y")))
 label3=Label(f_top, text="Место для общего времени тренировки", font="Arial 12", width=50, height=15, bg="#29b514")
 label4=Label(f_middle, text="Место для таймера тренировки", font="Arial 12", width=50, height=15, bg="red")
 label5=Label(f_middle, text="Кнопка начала тренировки", font="Arial 12", width=50, height=15, bg="#800606")
 label6=Label(f_bot, text="", font="Arial 12", width=50, height=15, bg="#800606")
+
 f_top.pack()
 f_middle.pack()
 f_bot.pack()
+
 label1.pack(side=LEFT, anchor=NW)
 label2.pack(side=LEFT, anchor=N)
 label3.pack(side=LEFT, anchor=NE)
